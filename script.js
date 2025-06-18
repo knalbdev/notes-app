d961eaff5d1b4811357bfdeb1a9f8ec3bf108bcb
Setianing Budi <setianingbudi2@gmail.com>
2025-06-18 10:19:25 +0700

    update title

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ class AppBar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header class="app-bar">
-        <h1>Notes App</h1>
+        <h1>Blank Notes App</h1>
       </header>
     `;
   }
